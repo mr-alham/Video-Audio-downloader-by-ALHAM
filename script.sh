ab271e764567ad59b86baad0af407634d25efd6f
alham__aa <alhamazhar4@gmail.com>
2024-02-25 22:42:46 +0530

    added some new functions to the script

--- script.sh
+++ script.sh
@@ -25,7 +25,7 @@ if ! [[ -d "${download_path}" ]]; then
 fi
 
 # download_path="/tmp/audio-video-downloader-by-alham/"
-log_file="${download_path}/.audio-video-downloader-by-alham.log"
+log_file="/var/log/Audio-Video-Downloader-by-ALHAM.log"
 temp_log_file='/tmp/audio-video-downloader-by-alham.log'
 
 error() {
@@ -134,7 +134,7 @@ default() {
     elif [[ "$decision" == 3 ]]; then  # display help
         text="Audio-Video downloader help menu"
         padding=$(( (columns - ${#text}) / 2 ))
-    
+
         echo -e "\n${bold}${green}${underlined}"
         printf "%*s%s%*s" "${padding}" "" "${text}" "${padding}"
         echo -e ""
@@ -164,36 +164,42 @@ help() {
     echo -e "\t\tdownload audio using the given URL"
     echo -e "\n\t${bold}${white}-V, --video${reset}${green}"
     echo -e "\t\tdownload video using the given URL"
+    echo -e "\n\t${bold}${white}-Vb, --video-best${reset}${green}"
+    echo -e "\t\tdownload video with the best format using the given URL"
 
     echo -e "\n${bold}USAGE${reset}${white}${bold}"
     echo -e "\t./script.sh --url <url>"
     echo -e "\t./script.sh -u <url>${reset}${green}"
     echo -e "\t\tparse the URL and the script will ask for other information interactively\n"
-    
+
     echo -e "\t${white}${bold}./script.sh --audio"
     echo -e "\t./script.sh -A ${reset}${green}"
     echo -e "\t\tdownload audio and the script will ask for other information interactively\n"
-    
+
     echo -e "\t${white}${bold}./script.sh --video"
     echo -e "\t./script.sh -V ${reset}${green}"
     echo -e "\t\tdownload video and the script will ask for other information interactively\n"
-    
-    echo -e "\t${white}${bold}./script.sh --audio --url"
+
+    echo -e "\t${white}${bold}./script.sh --audio --url <url>"
     echo -e "\t./script.sh -u <url> -A${reset}${green}"
     echo -e "\t\tdownload audio using the provided URL\n"
 
-    echo -e "\t${white}${bold}./script.sh --video --url"
+    echo -e "\t${white}${bold}./script.sh --video --url <url>"
     echo -e "\t./script.sh -u <url> -V${reset}${green}"
-    echo -e "\t\tdownload video using the provided URL and the script will ask which format you want to download interactively"
+    echo -e "\t\tdownload video using the provided URL and the script will ask which format you want to download interactively\n"
+
+    echo -e "\t${white}${bold}./script.sh --video-best --url <url>"
+    echo -e "\t./script.sh -u <url> -V${reset}${green}"
+    echo -e "\t\tdownload the best video format of provided URL"
 
     echo -e "\n${reset}${green}${bold}NOTE${reset}${green}"
-    echo -e "\tTo successfully use this script, you have to have '${white}yt-dlp${green}' and '${white}xclip${green}' installed if you need to copy a URL from the clipboard."
+    echo -e "\tTo successfully use this script, you have to have '${white}yt-dlp${green}' and '${white}xclip${green}'(to copy the URL from the clipboard) installed."
 
     echo -e "\n${bold}AUTHOR${reset}${green}"
-    echo -e "\tWritten by ${white}ALHAM${reset}${green}"
+    echo -e "\tWritten by: ${white}ALHAM${reset}${green}"
     echo -e "\tTwitter: ${white}https://www.twitter.com/@alham__aa${reset}${green}"
     echo -e "\tEmail: ${white}alham@duck.com\n${reset}"
-    
+
     log "done...\n"
     exit 0
 }
@@ -202,7 +208,7 @@ title() {
     title=$(yt-dlp --get-title "${url}" 2> ${temp_log_file})
     exit_code=$?
     log "fetching title ,$(cat ${temp_log_file} >/dev/null)"
-    
+
     if ! [[ ${exit_code} -eq 0 ]]; then
         log "Error: $? Could not get the title"
         echo -e "${red}Couldn't fetch the title"
@@ -212,10 +218,24 @@ title() {
     fi
 }
 
+notification(){
+    local title="Download Complete...!"
+    local message="$1" # message of notification (in here the tittle of the song)
+    local app_name="ALHAM"
+    local icon_path="/opt/alham_scripts/bash_scripts/Audio-Video-Downloader-by-ALHAM/file_download_done_FILL0_wght700_GRAD0_opsz48.svg"
+
+    # Display the notification
+    notify-send "$title" "<i>$message</i>" \
+        --app-name "$app_name" \
+        --icon $icon_path \
+        --urgency low \
+        -t 3000
+}
+
 download_audio() {
     # Logging the output
     log "Downloading audio"
-    
+
     # Change to the directory where you want to download the file
     # If the download path is incorrect, the script will download in the current working directory
     cd "${download_path}" 2> ${temp_log_file} && log "cd $download_path" || {
@@ -226,31 +246,31 @@ download_audio() {
     }
     exit_code=$?
     echo -e "\n${bold}${blue}${italic}Download Audio${reset}${green}"
-    
+
     # Check the URL
     url
-    
+
     # Fetch the title of the given URL
     title
-    
+
     # Get the file size
     file_size=$(yt-dlp -i --extract-audio --audio-format mp3 --audio-quality 0 --embed-thumbnail --get-filename --output '%(filesize)s' "$url" --no-progress --newline 2> $temp_log_file)
     exit_code=$?
     file_size=$(bc <<< "scale=2; $file_size/(1024*1024)")
-    
+
     echo -e "${blue}${italic}${file_size}MB${reset}${green}"
-    
+
     log "Fetching file size: $(cat ${temp_log_file})"
     log "File size: ${file_size}MB"
-    
+
     echo -e "${bold}\nDownloading${white}.${yellow}.${cyan}.${reset}${green}"
-    
+
     # Download the audio at the best quality
     # --audio-quality 0 means the best quality
     log "Downloading audio in progress..."
     yt-dlp -i --extract-audio --audio-format mp3 --audio-quality 0 --embed-thumbnail "${url}" --console-title --no-mtime 2> "${temp_log_file}" > /dev/null
     exit_code=$?
-    
+
     if ! [[ $? -eq 0 ]]; then
         exit_code=$?
         log "Error: $(cat ${temp_log_file}) An error occurred while downloading audio"
@@ -259,15 +279,25 @@ download_audio() {
         echo -e "${bold}Terminating...!${reset}${green}"
         exit 1
     fi
-    
+    notification "$title"
+
     echo -e "${bold}Done_\U0000270C ${reset}${green}"
-    
+
     exit_code=$?
     log "$(cat ${temp_log_file})"
     log "Done...\n"
-    
+
     # Exit from the script
     exit 0
+}
+
+download_video_best_quality(){
+    log "Downloading Video using best quality"
+
+    echo -e "\n${green}${bold}Downloading the video using available best format...${reset}"
+
+    yt-dlp --format "best" "$url" --console-title --no-mtime 2> ${temp_log_file} >/dev/null
+    exit_code=$?
 }
 
 download_video() {
@@ -285,22 +315,26 @@ download_video() {
     exit_code=$?
 
     echo -e "\n${bold}${blue}${italic}Download Video${reset}${green}"
-    
+
     # Check the URL
     url
-    
+
     # Fetch the title of the given URL's video
     title
-    
+
+    # Check whether need to download using best quality. else user can choose the video fomat
+    if [ "$video_best_flag" == true ]; then
+        download_video_best_quality
+    else
+
     echo -e "\n${bold}${green}[_]Select desired format:${reset}${green}"
-    
     l="${reset}${bold}${white}|${reset}${bold}${blue}"
     h="${bold}${white} ---  ----- ----------- ----------${reset}${cyan}"
-    
+
     echo -e "${reset}${white}${bold}$h\n${l}ID  ${l}EXT  ${l}FILESIZE   ${l}RESOLUTION ${l}${bold}${white}\n${h}${reset}${cyan}"
-    
+
     l="${reset}${bold}${white}|${reset}${cyan}"
-    
+
     # Fetch available formats for the video and show them on the terminal
     yt-dlp --list-formats "${url}" | awk -v pipe="${l}" -v blue="${blue}" -v bold="${bold}" '
         NR>8 && !/^(---|\[info\]|\[youtube\])/ && !/images|audio only/ && !/video only/ {
@@ -315,35 +349,43 @@ download_video() {
     if [[ $? -ne 0 ]]; then
         log "Error: $(cat ${temp_log_file}) An error occurred while getting video formats"
         echo -e "${red}An error occurred while getting video formats. Please try again.${reset}${green}"
-        log "done...\n"
-        echo -e "${bold}Terminating...!${reset}${green}"
-        exit 1
+        # log "done...\n"
+        # echo -e "${bold}Terminating...!${reset}${green}"
+        # exit 1
     fi
 
-    log "Fetching formats: $(cat ${temp_log_file})"
+        log "Fetching formats: $(cat ${temp_log_file})"
 
-    echo -e "${green}${bold}[_]Enter the ID of the video you want to download: ${reset}${green}"
-    read -rp "[ ] " format_id
+        echo -e "${green}${bold}[_]Enter the ID of the video you want to download: ${reset}${green}"
+        echo -e "${green}${bold}[_]${reset}${italic}${yellow}\"best\"${reset}${green}${bold} to download using best available quality:${reset}${green}"
+        read -rp "[ ] " format_id
 
-    echo -e "${bold}\nDownloading${white}.${yellow}.${cyan}.${reset}${green}"
+        if [ "${format_id}" == "best" ]; then
+            log "Selected format ID: ${format_id}"
+            download_video_best_quality
+        fi
 
-    log "Selected format ID: ${format_id}"
+        echo -e "${bold}\nDownloading${white}.${yellow}.${cyan}.${reset}${green}"
 
-    # Download the video using the given format
-    log "Downloading video..."
-    yt-dlp --format "$format_id" "$url" --console-title --no-mtime 2> ${temp_log_file} >/dev/null
-    exit_code=$?
+        log "Selected format ID: ${format_id}"
 
-    if [[ $? -ne 0 ]]; then
-        log "Error: $(cat ${temp_log_file}) An error occurred while downloading video"
-        echo -e "${red}An error occurred while downloading the video. Please try again.${reset}${green}"
-        log "done...\n"
-        echo -e "${bold}Terminating...!${reset}${green}"
-        exit 1
+        # Download the video using the given format
+        log "Downloading video..."
+        yt-dlp --format "$format_id" "$url" --console-title --no-mtime 2> ${temp_log_file} >/dev/null
+        exit_code=$?
+
+        if [[ $? -ne 0 ]]; then
+            log "Error: $(cat ${temp_log_file}) An error occurred while downloading video"
+            echo -e "${red}An error occurred while downloading the video. Please try again.${reset}${green}"
+            log "$(cat ${temp_log_file})"
+            log "done...\n"
+            echo -e "${bold}Terminating...!${reset}${green}"
+            exit 1
+        fi
     fi
 
+    notification "$title"
     exit_code=$?
-    log "$(cat ${temp_log_file})"
     log "done...\n"
 
     echo -e "${bold}Done_\U0000270C ${reset}${green}"
@@ -370,10 +412,10 @@ welcome() {
     text="Audio/Video Downloader by ALHAM"
     columns=$(tput cols)
     padding=$(( (columns - ${#text}) / 2 ))
-    
+
     echo -en "${bold}${yellow}${underlined}"
     printf "%*s%s%*s\n" "${padding}" "" "${text}" "${padding}" ""
-    
+
     # Checking if there are any arguments
     if [ $# -gt 0 ]; then
         # If an argument has a URL, get the URL from the arguments
@@ -384,8 +426,8 @@ welcome() {
                 url=${arg}
                 url_flag=false
                 break
-            fi    
-           
+            fi
+
             if [[ ${arg} == "--url" ]] || [[ ${arg} == "-u" ]]; then
                 log "script.sh ${arg}"
                 url_flag=true
@@ -393,19 +435,24 @@ welcome() {
         done
 
         # Check if arguments are specified
-        for arg in "$@"; do            
-           
+        for arg in "$@"; do
+            video_best_flag=false
             if [ "${arg}" = "--log" ]; then
                 exit_code=$?
                 log "script.sh --log"
                 cat "${log_file}"
                 log "done...\n"
-           
+
+            elif [ "${arg}" = "--video-best" ] || [ ${arg} = "-Vb" ]; then
+                log "script.sh ${arg}"
+                video_best_flag=true
+                download_video
+
             elif [ "${arg}" = "--video" ] || [ "${arg}" = "-V" ]; then
                 exit_code=$?
                 log "script.sh ${arg}"
                 download_video
-           
+
             elif [ "${arg}" = "--audio" ] || [ "${arg}" = "-A" ]; then
                 exit_code=$?
                 log "script.sh ${arg}"
